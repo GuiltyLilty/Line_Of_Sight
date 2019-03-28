06a1d3fd2171554f8cfdcad0c2ecc6a4467a8b90
Henry Church <pdbhenry@gmail.com>
2019-03-28 17:17:39 -0400

    Look Mechanic and Gizmos
    Look into deleting stranger if it's behind a wall.

---
+++ Line Of Sight/Assets/Scripts/strangerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class strangerController : MonoBehaviour {
+    bool seen = false;
+
+    void OnBecameVisible()
+    {
+        seen = true;
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
